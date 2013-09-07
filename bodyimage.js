18efeef125b64234b1e8edf1080e60586b123dc8
Jon Cuthbert <jon@cuth.net>
2013-09-07 13:50:50 -0400

    convert tabs to spaces

--- bodyimage.js
+++ bodyimage.js
@@ -1,10 +1,10 @@
 (function (w, d, $) {
-	"use strict";
-	w.BodyImage = function (el) {
-		this.result = this.init(el);
-	};
-	w.BodyImage.prototype = (function () {
-		var setImg = function (img) {
+    "use strict";
+    w.BodyImage = function (el) {
+        this.result = this.init(el);
+    };
+    w.BodyImage.prototype = (function () {
+        var setImg = function (img) {
                 img.resetFlag = false;
                 img.width = img.$el.width();
                 img.height = img.$el.height();
@@ -31,9 +31,9 @@
                 img.fullSize = true;
             },
             expandImage = function (index) {
-				var self = this,
+                var self = this,
                     img = this.img[index],
-					ratio, width, height, originX, originY, bodyX, bodyY, scale;
+                    ratio, width, height, originX, originY, bodyX, bodyY, scale;
                 if (img.resetFlag) {
                     setImg.call(this, img);
                 }
@@ -43,43 +43,43 @@
                 ratio = img.width / img.height;
                 width = this.win.width;
                 height = this.win.width / ratio;
-				if (height > this.win.height) {
-					height = this.win.height;
-					width = this.win.height * ratio;
-				}
-				originX = img.pos.left + img.width / 2;
-				originY = img.pos.top + img.height / 2;
-				scale = width / img.width;
-				bodyX = (originX - this.win.scrollLeft - this.win.width / 2);
-				bodyY = (originY - this.win.scrollTop - this.win.height / 2);
+                if (height > this.win.height) {
+                    height = this.win.height;
+                    width = this.win.height * ratio;
+                }
+                originX = img.pos.left + img.width / 2;
+                originY = img.pos.top + img.height / 2;
+                scale = width / img.width;
+                bodyX = (originX - this.win.scrollLeft - this.win.width / 2);
+                bodyY = (originY - this.win.scrollTop - this.win.height / 2);
                 if (!img.fullSize) {
                     fullSize.call(this, index);
                 }
-				this.$body.css({
-					width: this.win.width,
-					height: this.win.height,
-					overflow: 'hidden',
-					transformOrigin: (originX)+'px '+(originY)+'px',
-					transform: 'translate('+-bodyX+'px, '+-bodyY+'px) scale('+scale+')'
-				});
+                this.$body.css({
+                    width: this.win.width,
+                    height: this.win.height,
+                    overflow: 'hidden',
+                    transformOrigin: (originX)+'px '+(originY)+'px',
+                    transform: 'translate('+-bodyX+'px, '+-bodyY+'px) scale('+scale+')'
+                });
                 this.active = true;
-			},
+            },
             revertBody = function () {
                 this.active = false;
                 this.$body.css({
                     transform: 'translate(0,0) scale(1)'
                 });
             },
-			bindEvents = function () {
-				var self = this;
-				this.$el.bind('click', function (e) {
-					e.preventDefault();
+            bindEvents = function () {
+                var self = this;
+                this.$el.bind('click', function (e) {
+                    e.preventDefault();
                     if (self.active) {
                         revertBody.call(self);
                         return;
                     }
-					expandImage.call(self, self.$el.index(this));
-				});
+                    expandImage.call(self, self.$el.index(this));
+                });
                 this.$body.on('transitionend webkitTransitionEnd', function () {
                     if (!self.active) {
                         self.$body.removeAttr('style');
@@ -92,12 +92,12 @@
                     }
                     revertBody.call(self);
                 });
-			},
-			init = function (el) {
+            },
+            init = function (el) {
                 var self = this,
                     $w = $(w);
-				this.$el = $(el);
-				if (this.$el.length < 1) return false;
+                this.$el = $(el);
+                if (this.$el.length < 1) return false;
                 this.img = [];
                 this.$el.each(function (i) {
                     var img = self.img[i] = {};
@@ -105,15 +105,15 @@
                     img.fullSize = false;
                     setImg.call(self, img);
                 });
-				this.$body = $('body');
+                this.$body = $('body');
                 this.win = { $el: $w };
                 setWin.call(this);
                 this.active = false;
-				bindEvents.call(this);
-				return true;
-			};
-		return {
-			init: init
-		};
-	}());
+                bindEvents.call(this);
+                return true;
+            };
+        return {
+            init: init
+        };
+    }());
 }(window, document, jQuery));
