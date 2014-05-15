4b02d0be0a1007d7ebc49a78eebf6494c1583196
Jon Cuthbert <jon@cuth.net>
2014-05-14 23:59:15 -0400

    organized the functions better

--- bodyimage.js
+++ bodyimage.js
@@ -1,213 +1,253 @@
-(function (w, d, $) {
-    "use strict";
-    w.BodyImage = function (el, options) {
-        this.result = this.init(el, options);
+var BodyImage = (function ($) {
+    'use strict';
+
+    var isTouch = ('ontouchstart' in window);
+    var $window = $(window);
+    var $body = $('body');
+    var win = {};
+
+    var defaults = {
+        useArrowKeys: false,
+        windowLoad: true
     };
-    w.BodyImage.prototype = (function () {
-        var setDimensions = function () {
-                this.win.width = this.win.$el.width();
-                this.win.height = this.win.$el.height();
-                this.win.scrollLeft = this.win.$el.scrollLeft();
-                this.win.scrollTop = this.win.$el.scrollTop();
-                for (var x = 0, xlen = this.img.length; x < xlen; x += 1) {
-                    this.img[x].pos = this.img[x].$el.offset();
-                }
-                this.resetFlag = false;
-            },
-            setFullSizeImage = function (index) {
-                var img = this.img[index],
-                    temp = new Image();
-                temp.src = this.$el.eq(index).attr('href');
-                img.$el.css({
-                    width: img.width,
-                    height: img.height
-                });
-                $(temp).load(function () {
-                    img.$el.attr('src', temp.src);
-                });
-                img.fullSize = true;
-            },
-            expandImage = function (index) {
-                var self = this,
-                    img = this.img[index],
-                    width, height, originX, originY, bodyX, bodyY, scale;
-                if (this.resetFlag) {
-                    setDimensions.call(this);
-                }
-                width = Math.min(this.win.width, img.maxWidth);
-                height = width / img.ratio;
-                if (height > Math.min(this.win.height, img.maxHeight)) {
-                    height = Math.min(this.win.height, img.maxHeight);
-                    width = height * img.ratio;
-                }
-                originX = img.pos.left + img.outerWidth / 2;
-                originY = img.pos.top + img.outerHeight / 2;
-                scale = width / img.outerWidth;
-                bodyX = -(originX - this.win.scrollLeft - this.win.width / 2);
-                bodyY = -(originY - this.win.scrollTop - this.win.height / 2);
-                this.inTransition = true;
-                if (!self.isTouch) {
-                    this.$body.css({
-                        width: this.win.width,
-                        height: this.win.height,
-                        overflow: 'hidden'
-                    });
-                }
-                this.$body.css({
-                    transformOrigin: originX + 'px ' + originY + 'px',
-                    transform: 'translate3d(' + bodyX + 'px, ' + bodyY + 'px, 0) scale(' + scale + ')'
-                });
-                this.active = index;
-            },
-            revertBody = function () {
-                this.active = -1;
-                this.inTransition = true;
-                this.$body.css({
-                    transformOrigin: 'center center',
-                    transform: 'translate3d(0,0,0) scale(1)'
-                });
-            },
-            nextImage = function () {
-                var num = this.active + 1;
-                if (num >= this.img.length) {
-                    num = -1;
-                    revertBody.call(this);
-                    return;
-                }
-                expandImage.call(this, num);
-            },
-            previousImage = function () {
-                var num = this.active - 1;
-                if (num === -1) {
-                    revertBody.call(this);
-                    return;
-                }
-                if (num < -1) {
-                    num = this.img.length - 1;
-                }
-                expandImage.call(this, num);
-            },
-            bindEvents = function () {
-                var self = this;
-                this.$body.on('transitionend webkitTransitionEnd', function () {
-                    self.inTransition = false;
-                    if (self.active === -1) {
-                        self.$body.removeAttr('style');
-                        return;
-                    }
-                    if (!self.img[self.active].fullSize) {
-                        setFullSizeImage.call(self, self.active);
-                    }
-                });
-                this.$el.on('click', function (e) {
-                    console.log(e);
-                    e.preventDefault();
-                    e.stopPropagation();
-                    if (self.active >= 0) {
-                        revertBody.call(self);
-                        return false;
-                    }
-                    expandImage.call(self, self.$el.index(this));
-                });
-                this.$body.on('click', function () {
-                    if (self.active >= 0) {
-                        revertBody.call(self);
-                    }
-                });
-                this.win.$el.on('resize scroll', function (e) {
-                    if (e.type === 'scroll' && self.inTransition) return;
-                    self.resetFlag = true;
-                    if (!self.isTouch && self.active >= 0) {
-                        revertBody.call(self);
-                    }
-                });
-                this.$body.on('mousewheel', function (e) {
-                    if (self.active === -1) return true;
-                    if (e.originalEvent.wheelDelta < 0) {
-                        nextImage.call(self);
-                        return false;
-                    }
-                    if (e.originalEvent.wheelDelta > 0) {
-                        previousImage.call(self);
-                        return false;
-                    }
-                });
-                this.win.$el.on('keydown', function (e) {
-                    if (e.which === 27 && self.active >= 0) {
-                        revertBody.call(self);
-                    }
-                    if (!self.opts.useArrowKeys && self.active === -1) return true;
-                    if (e.which === 39) {
-                        nextImage.call(self);
-                    }
-                    if (e.which === 37) {
-                        previousImage.call(self);
-                    }
-                });
-            },
-            prepare = function () {
-                var self = this;
-                this.$el.each(function (i) {
-                    var $a = $(this),
-                        $img = $a.find('img'),
-                        width = $img.width(),
-                        height = $img.height(),
-                        outerWidth = $img.outerWidth(),
-                        outerHeight = $img.outerHeight(),
-                        ratio = outerWidth / outerHeight,
-                        attrWidth = parseInt($a.attr('data-width'), 10),
-                        attrHeight = parseInt($a.attr('data-height'), 10),
-                        maxWidth = 100000, maxHeight = 100000;
-                    if (attrWidth > 0) {
-                        maxWidth = attrWidth;
-                        maxHeight = attrWidth / ratio;
-                    } else if (attrHeight > 0) {
-                        maxHeight = attrHeight;
-                        maxWidth = attrHeight * ratio;
-                    }
-                    maxWidth += outerWidth - width;
-                    maxHeight += outerHeight - height;
-                    self.img[i] = {
-                        $el: $img,
-                        fullSize: false,
-                        width: width,
-                        height: height,
-                        outerWidth: outerWidth,
-                        outerHeight: outerHeight,
-                        ratio: ratio,
-                        maxWidth: maxWidth,
-                        maxHeight: maxHeight
-                    };
-                });
-                setDimensions.call(this);
-                bindEvents.call(this);
-            },
-            init = function (el, options) {
-                var self = this;
-                this.$el = $(el);
-                this.opts = $.extend({
-                    useArrowKeys: false,
-                    windowLoad: true
-                }, options);
-                if (this.$el.length < 1) return false;
-                this.isTouch = ('ontouchstart' in window);
-                this.win = { $el: $(w) };
-                this.$body = $('body');
-                this.resetFlag = false;
-                this.active = -1;
-                this.inTransition = false;
-                this.img = [];
-                if (this.opts.windowLoad) {
-                    this.win.$el.load(function () {
-                        prepare.call(self);
-                    });
-                } else {
-                    prepare.call(this);
-                }
-                return true;
+
+    var setFullSizeImage = function (index) {
+        var img = this.imgs[index],
+            temp = new Image();
+        temp.src = this.$triggers.eq(index).attr('href');
+        img.$el.css({
+            width: img.width,
+            height: img.height
+        });
+        $(temp).on('load', function () {
+            img.$el.attr('src', temp.src);
+        });
+        img.fullSize = temp;
+    };
+
+    var expandImage = function (index) {
+        var img = this.imgs[index],
+            width, height, originX, originY, bodyX, bodyY, scale;
+
+        if (this.resetFlag) {
+            setDimensions.call(this);
+        }
+
+        width = Math.min(win.width, img.maxWidth);
+        height = width / img.ratio;
+
+        if (height > Math.min(win.height, img.maxHeight)) {
+            height = Math.min(win.height, img.maxHeight);
+            width = height * img.ratio;
+        }
+
+        originX = img.pos.left + img.outerWidth / 2;
+        originY = img.pos.top + img.outerHeight / 2;
+
+        scale = width / img.outerWidth;
+
+        bodyX = -(originX - win.scrollLeft - win.width / 2);
+        bodyY = -(originY - win.scrollTop - win.height / 2);
+
+        this.inTransition = true;
+
+        if (!isTouch) {
+            $body.css({
+                width: win.width,
+                height: win.height,
+                overflow: 'hidden'
+            });
+        }
+
+        this.active = index;
+
+        $body.css({
+            transformOrigin: originX + 'px ' + originY + 'px',
+            transform: 'translate3d(' + bodyX + 'px, ' + bodyY + 'px, 0) scale(' + scale + ')'
+        });
+    };
+
+    var revertBody = function () {
+        this.active = -1;
+        this.inTransition = true;
+        $body.css({
+            transformOrigin: 'center center',
+            transform: 'translate3d(0,0,0) scale(1)'
+        });
+    };
+
+    var nextImage = function () {
+        var num = this.active + 1;
+        if (num >= this.imgs.length) {
+            num = -1;
+            revertBody.call(this);
+            return;
+        }
+        expandImage.call(this, num);
+    };
+
+    var previousImage = function () {
+        var num = this.active - 1;
+        if (num === -1) {
+            revertBody.call(this);
+            return;
+        }
+        if (num < -1) {
+            num = this.imgs.length - 1;
+        }
+        expandImage.call(this, num);
+    };
+
+    var onTransitionEnd = function (e) {
+        if (this.activeTransition === this.active) return;
+        this.activeTransition = this.active;
+
+        this.inTransition = false;
+        if (this.active === -1) {
+            $body.removeAttr('style');
+            return;
+        }
+        if (!this.imgs[this.active].fullSize) {
+            setFullSizeImage.call(this, this.active);
+        }
+    };
+
+    var onTriggerClick = function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        if (this.active >= 0) {
+            revertBody.call(this);
+            return false;
+        }
+        expandImage.call(this, this.$triggers.index(e.currentTarget));
+    };
+
+    var onBodyClick = function () {
+        if (this.active >= 0) {
+            revertBody.call(this);
+        }
+    };
+
+    var onResizeOrScroll = function (e) {
+        if (e.type === 'scroll' && this.inTransition) return;
+        this.resetFlag = true;
+        if (!isTouch && this.active >= 0) {
+            revertBody.call(this);
+        }
+    };
+
+    var onMouseWheel = function (e) {
+        if (this.active === -1) return true;
+        if (e.originalEvent.wheelDelta < 0) {
+            nextImage.call(this);
+            return false;
+        }
+        if (e.originalEvent.wheelDelta > 0) {
+            previousImage.call(this);
+            return false;
+        }
+    };
+
+    var onKeyDown = function (e) {
+        if (e.which === 27 && this.active >= 0) {
+            revertBody.call(this);
+            return;
+        }
+        if (!this.opts.useArrowKeys && this.active === -1) return true;
+        if (e.which === 39) {
+            nextImage.call(this);
+            return;
+        }
+        if (e.which === 37) {
+            previousImage.call(this);
+            return;
+        }
+    };
+
+    var bindEvents = function () {
+
+        $body.on('transitionend', onTransitionEnd.bind(this));
+
+        this.$triggers.on('click', onTriggerClick.bind(this));
+
+        $body.on('click', onBodyClick.bind(this));
+
+        $window.on('resize scroll', onResizeOrScroll.bind(this));
+
+        $body.on('mousewheel', onMouseWheel.bind(this));
+
+        $window.on('keydown', onKeyDown.bind(this));
+    };
+
+    var setDimensions = function () {
+        win.width = $window.width();
+        win.height = $window.height();
+        win.scrollLeft = $window.scrollLeft();
+        win.scrollTop = $window.scrollTop();
+
+        this.imgs = this.imgs.map(function (img) {
+            img.pos = img.$el.offset();
+            return img;
+        });
+
+        this.resetFlag = false;
+    };
+
+    var prepare = function () {
+        this.imgs = $.map(this.$triggers, function (trigger) {
+            var $trigger = $(trigger),
+                $img = $trigger.find('img'),
+                width = $img.width(),
+                height = $img.height(),
+                outerWidth = $img.outerWidth(),
+                outerHeight = $img.outerHeight(),
+                ratio = outerWidth / outerHeight,
+                attrWidth = parseInt($trigger.attr('data-width'), 10),
+                attrHeight = parseInt($trigger.attr('data-height'), 10),
+                maxWidth = 100000, maxHeight = 100000;
+
+            if (attrWidth > 0) {
+                maxWidth = attrWidth;
+                maxHeight = attrWidth / ratio;
+            } else if (attrHeight > 0) {
+                maxHeight = attrHeight;
+                maxWidth = attrHeight * ratio;
+            }
+
+            maxWidth += outerWidth - width;
+            maxHeight += outerHeight - height;
+
+            return {
+                '$el': $img,
+                'fullSize': false,
+                'width': width,
+                'height': height,
+                'outerWidth': outerWidth,
+                'outerHeight': outerHeight,
+                'ratio': ratio,
+                'maxWidth': maxWidth,
+                'maxHeight': maxHeight
             };
-        return {
-            init: init
-        };
-    }());
-}(window, document, jQuery));
+        });
+
+        setDimensions.call(this);
+        bindEvents.call(this);
+    };
+
+    return function (triggers, options) {
+        this.$triggers = $(triggers);
+        this.opts = $.extend({}, defaults, options);
+        if (this.$triggers.length < 1) return;
+
+        this.resetFlag = false;
+        this.active = -1;
+        this.activeTransition = -1;
+        this.inTransition = false;
+
+        if (this.opts.windowLoad) {
+            $window.on('load', prepare.bind(this));
+        } else {
+            prepare.call(this);
+        }
+    };
+}(jQuery));
